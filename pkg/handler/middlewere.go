2bc8cc42fd6655524bac911b6ea1aec6a9854fd0
Alexandr <navacar@mail.ru>
2025-01-07 15:12:09 +0200

    get lists methods

--- pkg/handler/middlewere.go
+++ pkg/handler/middlewere.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"errors"
 	"net/http"
 	"strings"
 
@@ -34,4 +35,20 @@ func (h *Handler) userIdentity(c *gin.Context) {
 	}
 
 	c.Set(userCtx, userId)
+}
+
+func getUserId(c *gin.Context) (int, error) {
+	id, ok := c.Get(userCtx)
+	if !ok {
+		newErrorResponse(c, http.StatusInternalServerError, "user id not found")
+		return 0, errors.New("user id not found")
+	}
+
+	idInt, ok := id.(int)
+	if !ok {
+		newErrorResponse(c, http.StatusInternalServerError, "user id is not valid type")
+		return 0, errors.New("user id not found")
+	}
+
+	return idInt, nil
 }
